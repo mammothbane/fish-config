34775067d93541f3325219dec0cd14e9939843c9
mammothbane <itsamammoth@gmail.com>
2015-08-21 03:22:31 -0400

    updated from deprecated usage

--- functions/dbrst.fish
+++ functions/dbrst.fish
@@ -1,3 +1,3 @@
 function dbrst 
-	heroku pg:reset DATABASE_URL --confirm clia-staging; and heroku rake db:schema:load
+	heroku pg:reset DATABASE_URL --confirm clia-staging; and heroku run rake db:schema:load
 end
