60d7317ebc17c35a67f8d3bc19be309954a01d4e
mammothbane <itsamammoth@gmail.com>
2015-08-25 00:26:51 -0400

    updated for rvm

---
+++ config.fish
@@ -0,0 +1,6 @@
+set __fish_git_prompt_describe_style branch
+set __fish_git_prompt_show_informative_status true
+set __fish_git_prompt_showcolorhints true
+set __fish_git_prompt_showupstream git verbose
+
+rvm default
